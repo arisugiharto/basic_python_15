b4784c20389ebc5d07a788c4500fbe7625464ac8
Ari Sugiharto <rarisugiharto@gmail.com>
2022-05-22 13:14:15 +0700

    test

--- user_input.py
+++ user_input.py
@@ -2,3 +2,11 @@
 
 nama = input("Siapa nama kamu: ")
 print(f"Halo, {nama}!")
+
+
+#input from user
+nama = input("Perkenalkan Nama kamu: ")
+print(f"Hallo, {nama} !")
+x = input("masukkan X: ")
+y = input("masukkan Y: ")
+print(f"Halo {nama}, Hasil Jumlah X + Y adalah: {int(x)+int(y)}")
